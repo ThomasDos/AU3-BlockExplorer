49d9c8f87e53110d69279d9347285c507cc9f7b9
Thomas Dos Anjos <dosanjos.thomas@gmail.com>
2023-05-22 08:46:31 +0200

    add link to block page in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <main>
-      <h1 className='text-xl text-center my-6'>Welcome in block explorer</h1>
+    <main className='text-center'>
+      <h1 className='text-xl my-6'>Welcome in block explorer</h1>
+      <Link href='/block'>
+        <span className='text-blue-600 hover:text-blue-900 cursor-pointer'>Visit the block page</span>
+      </Link>
     </main>
   )
 }
